1fee2bdfd10d5d7fc7daf8478eaad8e8719b4d60
Elena Piaggio <elenapiaggio@gmail.com>
2020-08-04 19:36:35 +0200

    ADD form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Fragment } from "react";
 import Header from "./components/Header";
+import Form from './components/Form';
 
 function App() {
   return (
     <Fragment>
       <Header title="Weather.app" />
+      <div className='contenedor-form'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col m6 s12'>
+              <Form/>
+            </div>
+            <div className='col m6 s12'>
+              the two
+            </div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
