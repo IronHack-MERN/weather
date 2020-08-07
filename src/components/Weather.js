70825e0bdca06430ccd0f0261556d94758b1aa66
Elena Piaggio <elenapiaggio@gmail.com>
2020-08-07 18:58:51 +0200

    ADD import  weathericons in class to use

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import WeatherIcons from 'react-weathericons';
 
 const Weather = ({ result }) => {
   const { name, main } = result;
